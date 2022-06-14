666aadcc13b71d7e0379d1b97584bc539373fa32
lwkas-gabriel <lwkasgabriel@hotmail.com>
2022-06-14 19:40:59 -0300

    commit inicial aulas react

--- 3-aulas-typescript/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-function somar(num1, num2) {
-    return num1 + num2;
-}
-console.log(somar(1, 5));
--- 3-aulas-typescript/index.ts
+++
@@ -1,5 +0,0 @@
-function somar(num1: number, num2: number) {
-    return num1 + num2;
-}
-
-console.log(somar(1, 5));
